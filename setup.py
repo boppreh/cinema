806501a4bb1ec818df273a98b68beba91130e369
BoppreH <boppreh@gmail.com>
2015-08-31 04:36:42 +0000

    Prefetch thumbnail

--- setup.py
+++ setup.py
@@ -15,22 +15,35 @@ def request_db(path, params={}):
     url = 'https://api.themoviedb.org/3{}?{}&api_key={}'.format(path, query, db_key)
     return json.loads(requests.get(url).text)
 
-base_image_url = request_db('/configuration')['images']['base_url'] + 'original'
+base_poster_url = request_db('/configuration')['images']['base_url'] + 'original'
+base_thumbnail_url = request_db('/configuration')['images']['base_url'] + 'w154'
+
 def ensure_overview(movie):
-    if movie.overview and movie.poster and movie.score:
+    if movie.overview and movie.poster and movie.score and movie.thumbnail:
         return
 
     response = request_db('/search/movie', {'query': movie.title})['results'][0]
     movie.overview = response['overview']
     movie.score = response['vote_average']
-    with movie._poster_path().open('wb') as f:
-        image_url = base_image_url + response['poster_path']
-        f.write(requests.get(image_url).content)
 
-    sleep(1)
+    if not movie.poster:
+        with movie._poster_path().open('wb') as f:
+            image_url = base_poster_url + response['poster_path']
+            f.write(requests.get(image_url).content)
+
+    if not movie.thumbnail:
+        try:
+            with movie._thumbnail_path().open('wb') as f:
+                image_url = base_thumbnail_url + response['poster_path']
+                f.write(requests.get(image_url).content)
+        except Exception:
+            movie.thumbnail.open('wb').write(movie.poster.open('rb').read())
+
+    sleep(0.1)
 
 print('Searching for movies...')
-movies = Movie.search(movies_dir, fetch_length=True, debug=True)
+#movies = Movie.search(movies_dir, fetch_length=True, debug=True)
+movies = load_cached_movies(movies_dir)
 
 print('Ensuring metadata...')
 for movie in movies[3:]:
