dd2c54cba21c4cf02394273eed0a1b478be514b8
BoppreH <boppreh@gmail.com>
2015-09-02 01:15:56 +0000

    More robust image downloading

--- setup.py
+++ setup.py
@@ -15,31 +15,41 @@ def request_db(path, params={}):
     url = 'https://api.themoviedb.org/3{}?{}&api_key={}'.format(path, query, db_key)
     return json.loads(requests.get(url).text)
 
-base_poster_url = request_db('/configuration')['images']['base_url'] + 'original'
-base_thumbnail_url = request_db('/configuration')['images']['base_url'] + 'w154'
+configuration = request_db('/configuration')
+poster_sizes = configuration['images']['poster_sizes']
+assert 'original' in poster_sizes
+base_image_url = configuration['images']['base_url']
+
+def try_download_image(size, final_url, path):
+    print('Trying to download image {} with size {} ({}).'.format(final_url, size, path.name))
+    assert size in poster_sizes
+    response = requests.get(base_image_url + size + '/' + final_url)
+    if response.status_code == 200:
+        with path.open('wb') as f:
+            f.write(response.content)
+            return True
+    return False
 
 def ensure_overview(movie):
-    if movie.overview and movie.poster and movie.score and movie.thumbnail:
+    if movie.overview and movie.poster and movie.score and movie.thumbnail and movie.backdrop:
         return
 
     response = request_db('/search/movie', {'query': movie.title})['results'][0]
     movie.overview = response['overview']
     movie.score = response['vote_average']
 
-    if not movie.poster:
-        with movie._poster_path().open('wb') as f:
-            image_url = base_poster_url + response['poster_path']
-            f.write(requests.get(image_url).content)
+    if movie.poster: movie.poster.unlink()
+    if movie.thumbnail: movie.thumbnail.unlink()
+    if movie.backdrop: movie.thumbnail.unlink()
 
-    if not movie.thumbnail:
-        try:
-            with movie._thumbnail_path().open('wb') as f:
-                image_url = base_thumbnail_url + response['poster_path']
-                f.write(requests.get(image_url).content)
-        except Exception:
-            movie.thumbnail.open('wb').write(movie.poster.open('rb').read())
+    try_download_image('original', response['poster_path'], movie._poster_path())
+    for size in poster_sizes:
+        success = try_download_image(size, response['poster_path'], movie._thumbnail_path())
+        if success:
+            break
+    try_download_image('original', response['backdrop_path'], movie._backdrop_path())
 
-    sleep(0.1)
+    sleep(0.3)
 
 print('Searching for movies...')
 #movies = Movie.search(movies_dir, fetch_length=True, debug=True)
@@ -49,7 +59,7 @@ print('Ensuring metadata...')
 for movie in movies[3:]:
     print(movie)
     try: ensure_overview(movie)
-    except Exception as e: print(e)
+    except Exception as e: print('--ERROR:', e)
     try: movie.ensure_subtitle('pt')
     except MissingSubtitle: pass
     try: movie.ensure_subtitle('en')
